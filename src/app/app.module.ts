b90a07120352eccd3424d5ab733bbf621ecb19d6
ocampo1979 <bato73@hotmail.com>
2017-11-05 22:42:39 -0500

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ColeccionistaComponent } from './coleccionista/coleccionista.component';
+import { MenuComponent } from './menu/menu.component';
+import { OpcionComponent } from './opcion/opcion.component';
+import { DiscosComponent } from './discos/discos.component';
+import { ColeccionistasComponent } from './coleccionistas/coleccionistas.component';
+import { AdicionarComponent } from './coleccionistas/adicionar/adicionar.component';
+import { IndexComponent } from './coleccionistas/index/index.component';
+import { ListaComponent } from './coleccionistas/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColeccionistaComponent,
+    MenuComponent,
+    OpcionComponent,
+    DiscosComponent,
+    ColeccionistasComponent,
+    AdicionarComponent,
+    IndexComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule
